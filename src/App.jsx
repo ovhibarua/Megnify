1db02c5009397e94248aa27ff93ce250738dfed6
ovhibarua <hey.itsmeovhi@gmail.com>
2023-06-20 14:02:37 +0600

    add text

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1>hello everyone</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod natus eligendi repellendus nam! Quod quo asperiores illum recusandae, velit repellendus obcaecati blanditiis doloribus accusantium impedit! Nulla odio excepturi mollitia libero!</p>
     </>
   )
 }
